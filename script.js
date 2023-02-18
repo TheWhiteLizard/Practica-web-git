8180adb3ce7487fd2c7d93d80dfe0157c00f24d5
Ricardo Chávez <75815255+TheWhiteLizard@users.noreply.github.com>
2023-02-18 15:10:28 -0600

    if and else

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ canvas.height = window.innerHeight;
 
 const pip = document.createElement('img');
 const boy = document.createElement('img');
+
+pip.src = 'images/pip.png';
+boy.src = 'images/boy.png';
+
+let paintbrush = pip;
+
+const context = canvas.getContext('2d');
+
+
+const handleMouseMove = (event) => {
+    const left = event.clientX;
+    const top = event.clientY;
+
+    context.drawImage(paintbrush, left, top);
+}
+
+const handleClick = () => {
+
+    if (paintbrush === pip){
+        paintbrush = boy;
+    } else {
+        paintbrush = pip
+    }
+
+}
+
+canvas.addEventListener('click', handleClick);
+canvas.addEventListener('mousemove', handleMouseMove);
