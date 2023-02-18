911830677c57904d65c9ff7dabc7e818a576493c
Ricardo Chávez <75815255+TheWhiteLizard@users.noreply.github.com>
2023-02-18 14:11:46 -0600

    New Images

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const canvas = document.querySelector('#drawingCanvas');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+const pip = document.createElement('img');
+const boy = document.createElement('img');
